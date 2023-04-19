fb5c718d5ea732772f4eb22746520429d224cf41
Yenleng <yenlengggggg@gmail.com>
2023-04-19 16:19:35 +0800

    git ignore edit

---
+++ src/main/java/com/nus/edu/iss/App.java
@@ -0,0 +1,13 @@
+package com.nus.edu.iss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
